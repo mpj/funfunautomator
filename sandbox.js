ccd97e718cbde4aba660b76b133889189bf04427
Mattias Petter Johansson <mpj@mpj.me>
2018-02-20 18:28:33 +0100

    sandbox impl of getting user

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,21 @@
 // list all discourse usernames
 const fetch = require('node-fetch')
 const key = process.env.DISCOURSE_API_KEY
+const sniff = require('supersniff')
+const R = require('ramda')
 
-
-const getAllUsers = require('./src/get-all-users')
-getAllUsers(0)
-  .then(console.log) // ?
-/*
+let username = 'jwalden'
 let x = fetch(
-  `https://www.funfunforum.com/admin/users/list/` +
-  `active.json?api_username=system&api_key=${key}&page=1`
-).then(x => x.json())
+  `https://www.funfunforum.com/users/${username}.json` +
+  `?api_username=system&api_key=${key}`
+)
+.then(x => x.json())
+.then(x => x.user.groups)
+.then(groups => groups.map(group => group.name))
+.then(sniff)
 
-x.then(users => users.map(user => user.username)) // ?
-*/
+//x.then(users => users.map(user => user.username)) // ?
+/**/
 /*
 ({ fetch, bus }) => {
   const
