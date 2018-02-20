469d23ee7cd20e0c99f3cf8aa8c4c12e0989650a
Mattias Petter Johansson <mpj@mpj.me>
2018-02-20 18:54:53 +0100

    made test more descriptive

--- src/get-groups/get-groups.test.js
+++ src/get-groups/get-groups.test.js
@@ -2,7 +2,22 @@ const getGroups = require('./get-groups')
 describe('getGroups', () => {
   it('happy path', () => {
     const deps = {
-      fetch: jest.fn(),
+      fetch: url => {
+        expect(url).toBe(
+          'https://mydiscourse.com/users/waffleman.json' +
+          `?api_username=system&api_key=secrit`
+        )
+        return Promise.resolve({
+          json: () => Promise.resolve({
+            user: {
+              groups: [
+                { name: 'group-a' },
+                { name: 'group-b' }
+              ]
+            }
+          })
+        })
+      },
       process: {
         env: {
           DISCOURSE_BASE_URI: 'https://mydiscourse.com',
@@ -10,16 +25,6 @@ describe('getGroups', () => {
         }
       }
     }
-    deps.fetch.mockReturnValueOnce(Promise.resolve({
-      json: () => Promise.resolve({
-        user: {
-          groups: [
-            { name: 'group-a' },
-            { name: 'group-b' }
-          ]
-        }
-      })
-    }))
     return getGroups(deps)('waffleman')
       .then(result => {
         expect(result[0]).toBe('group-a')
