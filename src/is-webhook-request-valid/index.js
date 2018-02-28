7182f25ead5b6a56544f567b9b9d370e573ea065
Mattias Petter Johansson <mpj@mpj.me>
2018-02-28 14:41:31 +0100

    copy paste programming

--- src/is-webhook-request-valid/index.js
+++ src/is-webhook-request-valid/index.js
@@ -1,7 +1,7 @@
 
 const crypto = require('crypto')
 
-function isWebhookRequestValid({ crypto, webhookSecret }, request) {
+function isWebhookRequestValid(request) {
   let hmac = crypto.createHmac('sha256', process.env.DISCOURSE_WEBHOOK_SECRET)
   let hash = `sha256=${hmac.update(request.rawBody).digest('hex')}`
   return hash === request.headers['x-discourse-event-signature']
