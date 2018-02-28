5ae9c72cff9b460c50f5e3d90618295f203f7d8a
Mattias Petter Johansson <mpj@mpj.me>
2018-02-28 13:39:05 +0100

    getQuery

---
+++ src/get-query/get-query.js
@@ -0,0 +1,13 @@
+const getQuery = ({
+  fetch,
+  process
+}) => id => {
+  const url =
+    `${process.env.DISCOURSE_BASE_URI}/admin/plugins/explorer/queries/${id}/run` +
+    `?api_username=system&api_key=${process.env.DISCOURSE_API_KEY}`
+  return fetch(url, { method: 'post' })
+    .then(response => response.json())
+}
+
+module.exports = getQuery
+
---
+++ src/get-query/get-query.test.js
@@ -0,0 +1,27 @@
+const getQuery = require('./get-query')
+describe('getQuery', () => {
+  it('happy path', () => {
+    const someResult = { a: 'b' }
+    const deps = {
+      fetch: (url, opts) => {
+        expect(url).toBe(
+          `https://ourdiscourse.com/admin/plugins/explorer/queries/5/run` +
+          `?api_username=system&api_key=secritz`
+        )
+        expect(opts.method).toBe('post')
+        return Promise.resolve({
+          json: () => Promise.resolve(someResult)
+        })
+      },
+      process: {
+        env: {
+          DISCOURSE_BASE_URI: 'https://ourdiscourse.com',
+          DISCOURSE_API_KEY: 'secritz'
+        }
+      }
+    }
+    return getQuery(deps)(5)
+      .then(result =>
+        expect(result).toBe(someResult))
+  })
+})
