fe5fee9b794fd2d5d82e8a35b528b07591595482
Mattias Petter Johansson <mpj@mpj.me>
2018-02-28 14:30:18 +0100

    webhook update implementation

--- server.js
+++ server.js
@@ -3,8 +3,65 @@ const cache = require('apicache').middleware
 const getHackableJSON = require('./src/get-hackable-json')
 const app = express()
 
-app.get('/hackablejson', cache('5 minutes'), (req, res) =>
-  getHackableJSON().then(res.json.bind(res)))
+const isWebhookRequestValid = require('./is-webhook-request-valid')
+
+let hackableJSONPromise
+function ensureHackableJSON() {
+  if (hackableJSONPromise) return
+  hackableJSONPromise = getHackableJSON()
+    .then(response =>
+      response.reduce((lookup, user) => ({
+        ...lookup,
+        ...{ [user.username] : user.hackablejson }
+      }), {}))
+    .then(cache => {
+      hackableJSONCache = cache
+    })
+  return hackableJSONPromise
+}
+
+let hackableJSONCache = {}
+
+app.get('/hackablejson', (req, res) => {
+  ensureHackableJSON()
+  res.json(hackableJSONCache)
+})
+
+app.post('/webhook', (req, res) => {
+  if (!isRequestValid(req)) {
+    res.status(403).send('invalid signature')
+    return
+  }
+
+  if(req.headers['x-discourse-event'] === 'user_updated') {
+
+    let hackableDataCache = state.cache.result.data
+    if (!hackableDataCache) {
+      res.status(200).send('carry on')
+    } else {
+      const HACKABLE_JSON_FIELD_ID = 1
+
+      const snapshot = {
+        username: req.body.user.username,
+        hackablejson:
+          userSnapshot.user_fields &&
+          userSnapshot.user_fields[''+HACKABLE_JSON_FIELD_ID]
+      }
+
+      if (!snapshot.hackablejson) {
+        // don't add this user, they might not want to be public
+        res.status(200).send('carry on')
+        return
+      }
+
+      hackableJSONCache[snapshot.username] = snapshot.hackablejson
+      res.status(200).send('cache updated')
+    }
+  } else {
+    res.status(200).send('event ignored')
+  }
+
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
---
+++ src/is-webhook-request-valid/index.js
@@ -0,0 +1,10 @@
+
+const crypto = require('crypto')
+
+function isWebhookRequestValid({ crypto, webhookSecret }, request) {
+  let hmac = crypto.createHmac('sha256', process.env.DISCOURSE_WEBHOOK_SECRET)
+  let hash = `sha256=${hmac.update(request.rawBody).digest('hex')}`
+  return hash === request.headers['x-discourse-event-signature']
+}
+
+module.exports = isWebhookRequestValid
