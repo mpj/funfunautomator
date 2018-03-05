a3cd33f1f4e5cdddb0ceb7125dd8868dcfb9a36a
Mattias Petter Johansson <mpj@mpj.me>
2018-03-05 16:50:09 +0100

    get-query params

--- src/get-query/get-query.js
+++ src/get-query/get-query.js
@@ -1,10 +1,11 @@
 const getQuery = ({
   fetch,
   process
-}) => id => {
+}) => (id, params) => {
   const url =
     `${process.env.DISCOURSE_BASE_URI}/admin/plugins/explorer/queries/${id}/run` +
-    `?api_username=system&api_key=${process.env.DISCOURSE_API_KEY}&limit=1000000`
+    `?api_username=system&api_key=${process.env.DISCOURSE_API_KEY}&limit=1000000` +
+    (params ? `&params=${JSON.stringify(params)}` : '')
   return fetch(url, { method: 'post' })
     .then(response => response.json())
 }
