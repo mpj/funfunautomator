b137626bb89914d9be53228a329bea6693e43ab4
Mattias Petter Johansson <mpj@mpj.me>
2018-03-13 18:17:40 +0100

    minor syntax change

--- src/is-webhook-request-valid.js
+++ src/is-webhook-request-valid.js
@@ -1,10 +1,7 @@
-
 const crypto = require('crypto')
 
-function isWebhookRequestValid(request) {
+module.exports = function isWebhookRequestValid(request) {
   let hmac = crypto.createHmac('sha256', process.env.DISCOURSE_WEBHOOK_SECRET)
   let hash = `sha256=${hmac.update(request.rawBody).digest('hex')}`
   return hash === request.headers['x-discourse-event-signature']
 }
-
-module.exports = isWebhookRequestValid
