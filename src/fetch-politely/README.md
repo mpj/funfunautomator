5bd9173471605173dc9784658b3f883d52bd184f
Mattias Petter Johansson <mpj@mpj.me>
2018-03-14 15:28:59 +0100

    fetchPolitely

---
+++ src/fetch-politely/README.md
@@ -0,0 +1,4 @@
+# fetchPolitely
+A wrapper around fetch that respects the rate limiting
+of the Discourse API. Behaves exactly like the normal
+fetch in all other regards
---
+++ src/fetch-politely/unbound.js
@@ -0,0 +1,10 @@
+module.exports = function fetchPolitely({ fetch, delay }, url, opts) {
+  return fetch(url, opts)
+    .then(response =>
+      response.status === 429 // = Too Many Requests
+        ? response.json()
+            .then(body => delay((body.extras.wait_seconds + 1) * 1000))
+            .then(() => fetchPolitely({ fetch, delay }, url, opts))
+        : response
+    )
+}
