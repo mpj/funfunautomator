c768e482585f6b45ec0f356510b8f236216f2330
Mattias Petter Johansson <mpj@mpj.me>
2018-03-14 15:49:19 +0100

    prettier

--- src/fetch-politely/test.js
+++ src/fetch-politely/test.js
@@ -3,33 +3,35 @@ const fetchPolitely = require('./unbound')
 describe('fetchPolitely', () => {
   it('waits when ratelimited', () => {
     let fetchCalledTimes = 0
-    return fetchPolitely({
-      fetch: (url, opts) => {
-        fetchCalledTimes++
-        expect(url).toBe(someUrl)
-        expect(opts).toBe(someOpts)
-        if (fetchCalledTimes === 1)
-          return Promise.resolve({
-            status: 429,
-            json: () => Promise.resolve({
-              extras: { wait_seconds: 14 }
+    return fetchPolitely(
+      {
+        fetch: (url, opts) => {
+          fetchCalledTimes++
+          expect(url).toBe(someUrl)
+          expect(opts).toBe(someOpts)
+          if (fetchCalledTimes === 1)
+            return Promise.resolve({
+              status: 429,
+              json: () =>
+                Promise.resolve({
+                  extras: { wait_seconds: 14 }
+                })
             })
-          })
-        else
-          return Promise.resolve(someResponse)
+          else return Promise.resolve(someResponse)
+        },
+        delay: ms => {
+          expect(ms).toBe((14 + 1) * 1000)
+          Promise.resolve()
+        }
       },
-      delay: ms => {
-        expect(ms).toBe((14 + 1) * 1000)
-        Promise.resolve()
-      }
-    }, someUrl, someOpts).then(response =>
-      expect(response).toBe(someResponse)
-    )
+      someUrl,
+      someOpts
+    ).then(response => expect(response).toBe(someResponse))
   })
 })
 
 const someUrl = 'http://myurl.com'
 const someOpts = { method: 'post' }
 let someResponse = {
-  status: 200,
+  status: 200
 }
--- src/fetch-politely/unbound.js
+++ src/fetch-politely/unbound.js
@@ -1,10 +1,11 @@
 module.exports = function fetchPolitely({ fetch, delay }, url, opts) {
-  return fetch(url, opts)
-    .then(response =>
+  return fetch(url, opts).then(
+    response =>
       response.status === 429 // = Too Many Requests
-        ? response.json()
+        ? response
+            .json()
             .then(body => delay((body.extras.wait_seconds + 1) * 1000))
             .then(() => fetchPolitely({ fetch, delay }, url, opts))
         : response
-    )
+  )
 }
