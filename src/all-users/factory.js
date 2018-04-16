12d4bf1139ccbb118481eaf2cb9ccf8f44c3ea59
Mattias Petter Johansson <mpj@mpj.me>
2018-04-16 15:25:16 +0200

    all-users refactor

--- src/all-users/factory.js
+++ src/all-users/factory.js
@@ -1,10 +1,9 @@
 module.exports = ({
-  fetch = require('node-fetch'),
+  fetch = require('node-fetch').default,
   process = global.process,
   junction = require('../junction/bypass')
 }) => page =>
   junction('fetch-body', () =>
-    //@ts-ignore
     fetch(
       process.env.DISCOURSE_BASE_URI +
         '/admin/users/list/active.json?api_username=system' +
